a991156b02220b748faedddc090c997845156089
FarahIsmahHana <farahismahhana01@gmail.com>
2022-06-17 22:16:54 +0700

    add test-logic in github

---
+++ index.js
@@ -0,0 +1,113 @@
+//1
+const number1 = (x) => {
+  let result = 1;
+  while (x > 0) {
+    result *= x;
+    x--;
+  }
+  return result;
+};
+
+// console.log(number1(4));
+// console.log(number1(8));
+
+//2
+let i = "abcde";
+const reverse = (str) => {
+  let result = "";
+  let length = str.length;
+  while (length > 0) {
+    result += str[length - 1];
+    length--;
+  }
+  return result;
+};
+// console.log(reverse(i));
+
+//3
+
+//4
+let a = 3;
+let b = 7;
+
+[a, b] = [b, a];
+
+// console.log(a);
+// console.log(b);
+
+//5
+const bilangan = (x) => {
+  let result = "";
+  let angka = ["", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+  if (x > 100 || x < 1) {
+    result = "Silahkan masukan angka 1-100";
+  } else {
+    if (x < 12) {
+      result = angka[x];
+    } else if (x > 11 && x < 20) {
+      result = angka[x % 10] + " Belas ";
+    } else if (x === 100) {
+      result = "Seratus";
+    } else {
+      result = angka[Math.floor(x / 10)] + " Puluh " + angka[x % 10];
+    }
+  }
+
+  return result;
+};
+
+// console.log(bilangan(4));
+// console.log(bilangan(20));
+// console.log(bilangan(39));
+// console.log(bilangan(104));
+
+//6
+const highLow = (arr, low, high) => {
+  result = arr.filter((n) => n > low && n < high);
+  return result;
+};
+
+// console.log(highLow([1, 4, 7, 9, 12], 2, 15));
+
+//7
+
+//8
+const eduwork = (x) => {
+  let i = 1;
+  while (i <= x) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log("Edu");
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log("Work");
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log("EduWork");
+    } else {
+      console.log(i);
+    }
+    i++;
+  }
+};
+
+// eduwork(15);
+
+//9
+const lowHigh = (arr) => {
+  let low = 0;
+  let high = 0;
+  arr.map((data) => {
+    if (data === arr[0]) {
+      high = data;
+      low = data;
+    } else {
+      if (data > high) {
+        high = data;
+      } else if (data < low) {
+        low = data;
+      }
+    }
+  });
+  console.log("low : ", low, " high : ", high);
+};
+// lowHigh([4, 2, 6, 88, 3, 11]);
+
+//10
